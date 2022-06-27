6d3caab9e4ceb2d41f56c1092b193aa3f1c61a48
Ivan <bykoviv78@gmail.com>
2022-06-27 17:56:30 +0700

    добавлен декодер/ 1

--- FromFile.py
+++ FromFile.py
@@ -4,7 +4,7 @@ import quopri
 
 from decoder import decode_str
 
-with open('source_file.eml', 'r', encoding='UTF-8') as in_file:
+with open('source_file1.eml', 'r', encoding='UTF-8') as in_file:
     #print(in_file.read())
     file_text = in_file.read()
 #print(file_text)
@@ -12,12 +12,23 @@ soup = BeautifulSoup(file_text, 'lxml')
 # print(soup)
 data = soup.find('table', class_='3D"w95"')
 #print(data)
-data.f
-name = data.find_all('td', class_='3D"font12"')
-for i in name:
+#data.f
+rows = data.find_all('tr')
+counter = 0
+for i in rows:
+    print(f'string # {counter}')
+    counter += 1
     print(decode_str(i.text))
-#print(decode_str(name))
-#name = data.find_all('td', class_='3D"font15"')
-#print(decode_str(name[0].text))
-#print(decode_str(name[1].text))
-#print(decode_str(name[2].text))
+#name = data.find_all('td', class_='3D"font12"')
+#counter = 0
+#for i in name:
+#    print(counter)
+#    counter += 1
+#    print(decode_str(i.text))
+#
+#digits = data.find_all('td', class_='3D"font15"')
+#counter = 0
+#for i in digits:
+#    print(counter)
+#    counter += 1
+#    print(decode_str(i.text))
