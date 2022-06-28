bd172de7768ea75df73185e0aedb79cd0bb19669
Ivan <bykoviv78@gmail.com>
2022-06-28 13:54:56 +0700

    готово

--- FromFile.py
+++ FromFile.py
@@ -1,34 +1,24 @@
 from bs4 import BeautifulSoup
-from chardet.universaldetector import UniversalDetector
-import quopri
 
 from decoder import decode_str
 
 with open('source_file1.eml', 'r', encoding='UTF-8') as in_file:
-    #print(in_file.read())
     file_text = in_file.read()
-#print(file_text)
 soup = BeautifulSoup(file_text, 'lxml')
-# print(soup)
 data = soup.find('table', class_='3D"w95"')
-#print(data)
-#data.f
+products = list()
 rows = data.find_all('tr')
-counter = 0
-for i in rows:
-    print(f'string # {counter}')
-    counter += 1
-    print(decode_str(i.text))
-#name = data.find_all('td', class_='3D"font12"')
-#counter = 0
-#for i in name:
-#    print(counter)
-#    counter += 1
-#    print(decode_str(i.text))
-#
-#digits = data.find_all('td', class_='3D"font15"')
-#counter = 0
-#for i in digits:
-#    print(counter)
-#    counter += 1
-#    print(decode_str(i.text))
+for row in rows[20:]:
+    split_row = decode_str(row.text).split('\n')
+    product = list()
+    for col in split_row:
+        if col.isspace() or col == '':
+            continue
+        else:
+            try:
+                product.append(float(col.strip()))
+            except ValueError:
+                product.append(col.strip())
+        if len(product) == 4 and type(product[1]) == float and type(product[2]) == float and type(product[3]) == float:
+            products.append(product)
+print(products)
